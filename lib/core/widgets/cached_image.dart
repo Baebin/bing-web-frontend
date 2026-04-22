2973e15bfa11fa9a49f7f75f2b50461608ee8167
Baebin <baebine@naver.com>
2026-04-22 23:41:53 +0900

    add flexible design and styling options to CachedImage

--- lib/core/widgets/cached_image.dart
+++ lib/core/widgets/cached_image.dart
@@ -1,21 +1,44 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 class CachedImage extends StatelessWidget {
   final String url;
+
   final double? width;
   final double? height;
   final BoxFit fit;
 
   final bool isCircle;
 
+  // Solid Border
+  final bool hasSolidBorder;
+
+  // Dotted Border
+  final bool hasDottedBorder;
+  final List<double> dashPattern;
+
+  // Border Options
+  final double borderWidth;
+  final Color borderColor;
+
   const CachedImage({
     super.key,
+
     required this.url,
+
     this.width,
     this.height,
     this.fit = BoxFit.cover,
     this.isCircle = false,
+
+    this.hasSolidBorder = false,
+
+    this.hasDottedBorder = false,
+    this.dashPattern = const [6, 3],
+
+    this.borderColor = Colors.black,
+    this.borderWidth = 2.0,
   });
 
   @override
@@ -41,6 +64,32 @@ class CachedImage extends StatelessWidget {
       );
     }
     if (isCircle) widget = ClipOval(child: widget);
+    if (hasSolidBorder) {
+      return Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          shape: isCircle ? BoxShape.circle : BoxShape.rectangle,
+          borderRadius: isCircle ? null : BorderRadius.circular(15),
+          border: Border.all(color: borderColor, width: borderWidth)
+        ),
+        child: isCircle ? widget : ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: widget,
+        ),
+      );
+    }
+    if (hasDottedBorder) {
+      return DottedBorder(
+        options: CircularDottedBorderOptions(
+          color: borderColor,
+          strokeWidth: borderWidth,
+          dashPattern: dashPattern,
+          padding: const EdgeInsets.all(4),
+        ),
+        child: widget,
+      );
+    }
     return widget;
   }
 }
