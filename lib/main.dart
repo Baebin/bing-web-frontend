5eb869017b5bd903ae6bd338c14cb17fcb9a555d
Baebin <baebine@naver.com>
2026-04-22 22:57:20 +0900

    enable URL path modification

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:bing_web_frontend/core/constants/app_colors.dart';
 import 'package:bing_web_frontend/core/router/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
+  // Edit URL
+  GoRouter.optionURLReflectsImperativeAPIs = true;
+  // Remove # from URL
+  // usePathUrlStrategy();
+
   runApp(
     const ProviderScope(child: MyApp()),
   );
