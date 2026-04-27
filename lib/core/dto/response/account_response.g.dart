ef18815a62a6affdaec7f84e6981974838bd99fd
Baebin <baebine@naver.com>
2026-04-27 21:24:26 +0900

    feat: add generated freezed files for data models

---
+++ lib/core/dto/response/account_response.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'account_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_AccountResponse _$AccountResponseFromJson(Map<String, dynamic> json) =>
+    _AccountResponse(
+      id: json['id'] as String,
+      email: json['email'] as String,
+      regDate: json['reg_date'] as String,
+    );
+
+Map<String, dynamic> _$AccountResponseToJson(_AccountResponse instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'email': instance.email,
+      'reg_date': instance.regDate,
+    };
